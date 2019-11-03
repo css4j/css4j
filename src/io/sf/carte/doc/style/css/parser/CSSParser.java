45967c807a8b288a77dd508faa04076303ecec0e
Carlos Amengual <carlosame644+github@gmail.com>
2019-11-03 18:28:46 +0100

    NSAC impl.: do not include CDO-CDC comments in 'ignorable' at-rules.

--- src/io/sf/carte/doc/style/css/parser/CSSParser.java
+++ src/io/sf/carte/doc/style/css/parser/CSSParser.java
@@ -2767,7 +2767,9 @@ public class CSSParser implements Parser2 {
 		public void commented(int index, int commentType, String comment) {
 			if (stage == 5 || stage == 7) {
 				// Unknown rule
-				buffer.append("/*").append(comment).append("*/");
+				if (commentType == 0) {
+					buffer.append("/*").append(comment).append("*/");
+				}
 			} else if (contextHandler != null) {
 				contextHandler.commented(index, commentType, comment);
 			} else {
