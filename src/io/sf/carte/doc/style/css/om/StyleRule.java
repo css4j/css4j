81c4a8d182344da310415e18ff3f36fa863da1e4
Carlos Amengual <carlosame644+github@gmail.com>
2020-03-28 17:59:29 +0100

    CSSOM [refactor]: move specificity class to its own file.

--- src/io/sf/carte/doc/style/css/om/StyleRule.java
+++ src/io/sf/carte/doc/style/css/om/StyleRule.java
@@ -21,17 +21,10 @@ import org.w3c.dom.DOMException;
 import io.sf.carte.doc.style.css.CSSRule;
 import io.sf.carte.doc.style.css.CSSStyleRule;
 import io.sf.carte.doc.style.css.SelectorMatcher;
-import io.sf.carte.doc.style.css.nsac.ArgumentCondition;
 import io.sf.carte.doc.style.css.nsac.CSSException;
-import io.sf.carte.doc.style.css.nsac.CombinatorCondition;
-import io.sf.carte.doc.style.css.nsac.CombinatorSelector;
-import io.sf.carte.doc.style.css.nsac.Condition;
-import io.sf.carte.doc.style.css.nsac.ConditionalSelector;
-import io.sf.carte.doc.style.css.nsac.ElementSelector;
 import io.sf.carte.doc.style.css.nsac.Parser;
 import io.sf.carte.doc.style.css.nsac.Selector;
 import io.sf.carte.doc.style.css.nsac.SelectorList;
-import io.sf.carte.doc.style.css.nsac.SimpleSelector;
 
 /**
  * CSS style rule.
@@ -83,181 +76,13 @@ public class StyleRule extends CSSStyleDeclarationRule implements CSSStyleRule {
 	 * 
 	 * @return the specificity.
 	 */
-	RuleSpecifity getSpecifity(int index, SelectorMatcher matcher) {
-		return new RuleSpecifity(getSelectorList().item(index), matcher);
+	RuleSpecificity getSpecificity(int index, SelectorMatcher matcher) {
+		return new RuleSpecificity(getSelectorList().item(index), matcher);
 	}
 
-	/**
-	 * The specificity of this rule.
-	 * <p>
-	 * See Cascading Style Sheets, level 2 revision 1 CSS 2.1 Specification,
-	 * paragraph 6.4.3.
-	 */
-	static class Specifity {
+	class RuleSpecificity extends Specificity {
 
-		short id_count = 0;
-
-		short attrib_classes_count = 0;
-
-		short names_pseudoelements_count = 0;
-
-		private final SelectorMatcher selectorMatcher;
-
-		public Specifity(Selector selector, SelectorMatcher matcher) {
-			super();
-			this.selectorMatcher = matcher;
-			specifity(selector);
-		}
-
-		private void setSpecificityFrom(Specifity specifity) {
-			id_count = specifity.id_count;
-			attrib_classes_count = specifity.attrib_classes_count;
-			names_pseudoelements_count = specifity.names_pseudoelements_count;
-		}
-
-		private void clear() {
-			id_count = 0;
-			attrib_classes_count = 0;
-			names_pseudoelements_count = 0;
-		}
-
-		private void specifity(Selector selector) {
-			switch (selector.getSelectorType()) {
-			case ELEMENT:
-				String elname = ((ElementSelector) selector).getLocalName();
-				if (elname == null || elname.equals("*")) {
-					// "ignore the universal selector"
-					break;
-				}
-				names_pseudoelements_count++;
-				break;
-			case CONDITIONAL:
-				ConditionalSelector condsel = (ConditionalSelector) selector;
-				conditionSpecificity(condsel.getCondition(), condsel.getSimpleSelector(), this);
-				break;
-			case DESCENDANT:
-			case CHILD:
-			case DIRECT_ADJACENT:
-				specifity(((CombinatorSelector) selector).getSecondSelector());
-				specifity(((CombinatorSelector) selector).getSelector());
-				break;
-			default:
-			}
-		}
-
-		private void conditionSpecificity(Condition cond, SimpleSelector selector, Specifity sp) {
-			switch (cond.getConditionType()) {
-			case CLASS:
-			case ATTRIBUTE:
-			case ONE_OF_ATTRIBUTE:
-			case BEGIN_HYPHEN_ATTRIBUTE:
-			case BEGINS_ATTRIBUTE:
-			case ENDS_ATTRIBUTE:
-			case SUBSTRING_ATTRIBUTE:
-			case PSEUDO_CLASS:
-			case LANG:
-			case ONLY_CHILD:
-			case ONLY_TYPE:
-			case POSITIONAL:
-				sp.attrib_classes_count++;
-				break;
-			case PSEUDO_ELEMENT:
-				sp.names_pseudoelements_count++;
-				break;
-			case ID:
-				sp.id_count++;
-				break;
-			case AND:
-				CombinatorCondition comb = (CombinatorCondition) cond;
-				Specifity firstsp = new Specifity(selector, selectorMatcher);
-				conditionSpecificity(comb.getFirstCondition(), selector, firstsp);
-				Specifity secondsp = new Specifity(selector, selectorMatcher);
-				conditionSpecificity(comb.getSecondCondition(), selector, secondsp);
-				if (firstsp.id_count > secondsp.id_count) {
-					sp.id_count += firstsp.id_count;
-					sp.attrib_classes_count += firstsp.attrib_classes_count;
-					sp.names_pseudoelements_count += firstsp.names_pseudoelements_count;
-				} else if (firstsp.id_count < secondsp.id_count) {
-					sp.id_count += secondsp.id_count;
-					sp.attrib_classes_count += secondsp.attrib_classes_count;
-					sp.names_pseudoelements_count += secondsp.names_pseudoelements_count;
-				} else {
-					if (firstsp.attrib_classes_count > secondsp.attrib_classes_count) {
-						sp.attrib_classes_count += firstsp.attrib_classes_count;
-						sp.names_pseudoelements_count += firstsp.names_pseudoelements_count;
-					} else if (firstsp.attrib_classes_count < secondsp.attrib_classes_count) {
-						sp.attrib_classes_count += secondsp.attrib_classes_count;
-						sp.names_pseudoelements_count += secondsp.names_pseudoelements_count;
-					} else {
-						if (firstsp.names_pseudoelements_count > secondsp.names_pseudoelements_count) {
-							sp.names_pseudoelements_count += firstsp.names_pseudoelements_count;
-						} else if (firstsp.names_pseudoelements_count < secondsp.names_pseudoelements_count) {
-							sp.names_pseudoelements_count += secondsp.names_pseudoelements_count;
-						}
-					}
-				}
-				return;
-			case SELECTOR_ARGUMENT:
-				ArgumentCondition acond = (ArgumentCondition) cond;
-				SelectorList argList = acond.getSelectors();
-				int selIdx = selectorMatcher.matches(argList);
-				if (selIdx == -1) {
-					clear();
-					return;
-				}
-				Specifity argSpecificity = new Specifity(argList.item(selIdx), selectorMatcher);
-				setSpecificityFrom(argSpecificity);
-				break;
-			default:
-			}
-			sp.specifity(selector);
-		}
-
-		@Override
-		public int hashCode() {
-			final int prime = 31;
-			int result = 1;
-			result = prime * result + attrib_classes_count;
-			result = prime * result + id_count;
-			result = prime * result + names_pseudoelements_count;
-			return result;
-		}
-
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj)
-				return true;
-			if (obj == null)
-				return false;
-			if (!(obj instanceof Specifity))
-				return false;
-			Specifity other = (Specifity) obj;
-			if (attrib_classes_count != other.attrib_classes_count)
-				return false;
-			if (id_count != other.id_count)
-				return false;
-			if (names_pseudoelements_count != other.names_pseudoelements_count)
-				return false;
-			return true;
-		}
-
-		static int selectorCompare(Specifity o1, Specifity o2) {
-			return (o1.id_count - o2.id_count) * 16384 + (o1.attrib_classes_count - o2.attrib_classes_count) * 128
-					+ (o1.names_pseudoelements_count - o2.names_pseudoelements_count);
-		}
-
-		@Override
-		public String toString() {
-			StringBuilder buf = new StringBuilder(82);
-			buf.append("id: ").append(id_count).append(", attributes and pseudo-classes: ").append(attrib_classes_count)
-					.append(", element names and pseudo-elements: ").append(names_pseudoelements_count);
-			return buf.toString();
-		}
-	}
-
-	class RuleSpecifity extends Specifity {
-
-		public RuleSpecifity(Selector selector, SelectorMatcher matcher) {
+		public RuleSpecificity(Selector selector, SelectorMatcher matcher) {
 			super(selector, matcher);
 		}
 
@@ -274,7 +99,7 @@ public class StyleRule extends CSSStyleDeclarationRule implements CSSStyleRule {
 				return false;
 			if (getClass() != obj.getClass())
 				return false;
-			if (getCSSStyleRule().getOrigin() != ((RuleSpecifity) obj).getCSSStyleRule().getOrigin())
+			if (getCSSStyleRule().getOrigin() != ((RuleSpecificity) obj).getCSSStyleRule().getOrigin())
 				return false;
 			return true;
 		}
@@ -293,7 +118,7 @@ public class StyleRule extends CSSStyleDeclarationRule implements CSSStyleRule {
 	 * This comparator ignores rule insertion index, so any users of this class
 	 * have to deal with that elsewhere.
 	 */
-	static class SpecificityComparator implements Comparator<RuleSpecifity> {
+	static class SpecificityComparator implements Comparator<RuleSpecificity> {
 
 		/*
 		 * Compares the two arguments for order. <p> Returns a negative integer,
@@ -301,9 +126,9 @@ public class StyleRule extends CSSStyleDeclarationRule implements CSSStyleRule {
 		 * to, or greater than the second.
 		 */
 		@Override
-		public int compare(RuleSpecifity o1, RuleSpecifity o2) {
+		public int compare(RuleSpecificity o1, RuleSpecificity o2) {
 			return (o2.getCSSStyleRule().getOrigin() - o1.getCSSStyleRule().getOrigin()) * 131071
-					+ Specifity.selectorCompare(o1, o2);
+					+ Specificity.selectorCompare(o1, o2);
 		}
 	}
 
