319737b9ffb210288f25e8cb3d8a3d7b25bfc553
Carlos Amengual <carlosame644+github@gmail.com>
2020-10-12 18:39:23 +0200

    DOM: Attr.getTextContent() should return the attribute value.
    + additional getTextContent tests.
    
    See https://dom.spec.whatwg.org/#dom-node-textcontent
    
    Instead, the Javadoc was suggesting otherwise (and that was the previous behaviour here).

--- src/io/sf/carte/doc/dom/AbstractDOMNode.java
+++ src/io/sf/carte/doc/dom/AbstractDOMNode.java
@@ -640,7 +640,6 @@ abstract class AbstractDOMNode implements DOMNode, java.io.Serializable {
 		String text;
 		switch (getNodeType()) {
 		case Node.ELEMENT_NODE:
-		case Node.ATTRIBUTE_NODE:
 		case Node.ENTITY_NODE:
 		case Node.ENTITY_REFERENCE_NODE:
 		case Node.DOCUMENT_FRAGMENT_NODE:
@@ -677,6 +676,7 @@ abstract class AbstractDOMNode implements DOMNode, java.io.Serializable {
 		case Node.CDATA_SECTION_NODE:
 		case Node.COMMENT_NODE:
 		case Node.PROCESSING_INSTRUCTION_NODE:
+		case Node.ATTRIBUTE_NODE:
 			text = getNodeValue();
 			break;
 		default:
