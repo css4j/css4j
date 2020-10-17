83cd2e0efc9cc60fe28c2af0857d1385d33de765
Carlos Amengual <carlosame644+github@gmail.com>
2020-10-17 17:27:03 +0200

    DOM: handle META referrer attribute values case-insensitively.

--- src/io/sf/carte/doc/dom/DOMDocument.java
+++ src/io/sf/carte/doc/dom/DOMDocument.java
@@ -2687,7 +2687,7 @@ abstract public class DOMDocument extends DOMParentNode implements CSSDocument {
 		if ("default-style".equalsIgnoreCase(name)) {
 			metaDefaultStyleSet = attribute;
 			onSheetModify();
-		} else if ("referrer".equals(name)) {
+		} else if ("referrer".equalsIgnoreCase(name)) {
 			metaReferrerPolicy = attribute;
 		}
 	}
@@ -2705,7 +2705,7 @@ abstract public class DOMDocument extends DOMParentNode implements CSSDocument {
 	public void onMetaRemoved(String name, String attribute) {
 		if ("default-style".equalsIgnoreCase(name)) {
 			metaDefaultStyleSet = "";
-		} else if ("referrer".equals(name)) {
+		} else if ("referrer".equalsIgnoreCase(name)) {
 			metaReferrerPolicy = "";
 		}
 	}
