e2c9c4445a4cd6708f2ba7d8c60892541b07dee0
Carlos Amengual <carlosame644+github@gmail.com>
2020-11-12 18:39:57 +0100

    Tests: create InputSource from InputStreamReader in a few DOM wrapper tests.

--- junit/io/sf/carte/doc/style/css/om/StylableDocumentWrapperTest2.java
+++ junit/io/sf/carte/doc/style/css/om/StylableDocumentWrapperTest2.java
@@ -19,6 +19,8 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -147,7 +149,7 @@ public class StylableDocumentWrapperTest2 {
 		Document doc;
 		try {
 			docbuilder.setEntityResolver(new DefaultEntityResolver());
-			InputSource source = new InputSource(is);
+			InputSource source = new InputSource(new InputStreamReader(is, StandardCharsets.UTF_8));
 			doc = docbuilder.parse(source);
 		} finally {
 			is.close();
@@ -170,7 +172,7 @@ public class StylableDocumentWrapperTest2 {
 		Document doc;
 		try {
 			docbuilder.setEntityResolver(new DefaultEntityResolver());
-			InputSource source = new InputSource(is);
+			InputSource source = new InputSource(new InputStreamReader(is, StandardCharsets.UTF_8));
 			doc = docbuilder.parse(source);
 		} finally {
 			is.close();
