9172445a13a7bfed77a5097fe7e83ec15c863b58
Carlos Amengual <carlosame644+github@gmail.com>
2020-12-01 22:55:03 +0100

    DOM: add basic support for a simple SVGElement interface.

---
+++ junit/io/sf/carte/doc/geom/RectTest.java
@@ -0,0 +1,51 @@
+/*
+
+ Copyright (c) 2005-2020, Carlos Amengual.
+
+ SPDX-License-Identifier: BSD-3-Clause
+
+ Licensed under a BSD-style License. You can find the license here:
+ https://css4j.github.io/LICENSE.txt
+
+ */
+
+package io.sf.carte.doc.geom;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class RectTest {
+
+	@Test
+	public void testRect() {
+		Rect rect = new Rect();
+		assertEquals(0d, rect.getX(), 1e-15);
+		rect.setX(8d);
+		assertEquals(8d, rect.getX(), 1e-15);
+		rect = new Rect(8, 12, 40, 30);
+		assertEquals(8d, rect.getX(), 1e-15);
+		assertEquals(12d, rect.getY(), 1e-15);
+		assertEquals(40d, rect.getWidth(), 1e-15);
+		assertEquals(30d, rect.getHeight(), 1e-15);
+		rect.setY(4d);
+		assertEquals(4d, rect.getY(), 1e-15);
+		rect.setWidth(160d);
+		assertEquals(160d, rect.getWidth(), 1e-15);
+		rect.setHeight(90d);
+		assertEquals(90d, rect.getHeight(), 1e-15);
+		//
+		assertEquals(4d, rect.getTop(), 1e-15);
+		assertEquals(168d, rect.getRight(), 1e-15);
+		assertEquals(94d, rect.getBottom(), 1e-15);
+		assertEquals(8d, rect.getLeft(), 1e-15);
+		//
+		rect.setWidth(-160d);
+		rect.setHeight(-90d);
+		assertEquals(-86d, rect.getTop(), 1e-15);
+		assertEquals(8d, rect.getRight(), 1e-15);
+		assertEquals(4d, rect.getBottom(), 1e-15);
+		assertEquals(-152d, rect.getLeft(), 1e-15);
+	}
+
+}
---
+++ src/io/sf/carte/doc/geom/Rect.java
@@ -0,0 +1,93 @@
+/*
+
+ Copyright (c) 2020, Carlos Amengual.
+
+ SPDX-License-Identifier: BSD-3-Clause
+
+ Licensed under a BSD-style License. You can find the license here:
+ https://css4j.github.io/LICENSE.txt
+
+ */
+
+package io.sf.carte.doc.geom;
+
+/**
+ * Implementation of {@link DOMRect}.
+ */
+public class Rect implements DOMRect {
+
+	private double x, y, width, height;
+
+	public Rect() {
+		this(0, 0, 0, 0);
+	}
+
+	public Rect(double x, double y, double width, double height) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+	}
+
+	@Override
+	public double getX() {
+		return x;
+	}
+
+	@Override
+	public void setX(double value) {
+		this.x = value;
+	}
+
+	@Override
+	public double getY() {
+		return y;
+	}
+
+	@Override
+	public void setY(double value) {
+		this.y = value;
+	}
+
+	@Override
+	public double getWidth() {
+		return width;
+	}
+
+	@Override
+	public void setWidth(double width) {
+		this.width = width;
+	}
+
+	@Override
+	public double getHeight() {
+		return height;
+	}
+
+	@Override
+	public void setHeight(double height) {
+		this.height = height;
+	}
+
+	@Override
+	public double getTop() {
+		return Math.min(y, y + height);
+	}
+
+	@Override
+	public double getRight() {
+		return Math.max(x, x + width);
+	}
+
+	@Override
+	public double getBottom() {
+		return Math.max(y, y + height);
+	}
+
+	@Override
+	public double getLeft() {
+		return Math.min(x, x + width);
+	}
+
+}
