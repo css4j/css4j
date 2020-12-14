fadd170221a913afd66c76ac660bfbf3f6ea1eff
Carlos Amengual <carlosame644+github@gmail.com>
2020-12-14 19:28:47 +0100

    Follow-up to [bab977a9]: remove a few tests that weren't intended for 1-stable.

--- junit/io/sf/carte/doc/AllTests.java
+++ junit/io/sf/carte/doc/AllTests.java
@@ -30,6 +30,7 @@ import io.sf.carte.doc.dom.IteratorTest;
 import io.sf.carte.doc.dom.NodeIteratorTest;
 import io.sf.carte.doc.dom.TreeWalkerTest;
 import io.sf.carte.doc.dom.XHTMLDocumentTest;
+import io.sf.carte.doc.dom.XMLDocumentBuilderJdkTest;
 import io.sf.carte.doc.dom.XMLDocumentBuilderTest;
 import io.sf.carte.doc.dom.XMLDocumentTest;
 import io.sf.carte.doc.style.css.SACParserFactoryTest;
@@ -155,6 +156,7 @@ import io.sf.carte.util.BufferSimpleWriterTest;
 	CollectionsTest.class,
 	TreeWalkerTest.class,
 	XHTMLDocumentTest.class,
+	XMLDocumentBuilderJdkTest.class,
 	XMLDocumentBuilderTest.class,
 	XMLDocumentTest.class,
 	AbstractStyleDatabaseTest.class,
