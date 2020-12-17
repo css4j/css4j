3cb0f0c95433e37abf8c8e4bb9040079a66c9434
Carlos Amengual <carlosame644+github@gmail.com>
2020-12-17 23:58:20 +0100

    Javadocs: add comment to CSSPageRule.getMarginRules().
    (backported from commit 3eb7c1bc10f693a3d58b0b035ce6f6ef1de33b13)

--- src/io/sf/carte/doc/style/css/ExtendedCSSPageRule.java
+++ src/io/sf/carte/doc/style/css/ExtendedCSSPageRule.java
@@ -4,7 +4,7 @@
  * Copyright © 2016 W3C® (MIT, ERCIM, Keio, Beihang).
  * https://www.w3.org/Consortium/Legal/2015/copyright-software-and-document
  *
- * Copyright © 2019 Carlos Amengual.
+ * Copyright © 2019,2020 Carlos Amengual.
  *
  * SPDX-License-Identifier: W3C-20150513
  */
@@ -19,6 +19,12 @@ import org.w3c.dom.css.CSSPageRule;
  */
 public interface ExtendedCSSPageRule extends CSSDeclarationRule, CSSPageRule {
 
+	/**
+	 * Get the list of page-margin rules.
+	 * 
+	 * @return the list of page-margin rules, or {@code null} if there are no margin
+	 *         rules.
+	 */
 	ExtendedCSSRuleList<? extends CSSMarginRule> getMarginRules();
 
 }
