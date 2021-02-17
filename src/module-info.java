33fc17d43416139bc2f0e904b449e64308d37f5e
Carlos Amengual <carlosame644+github@gmail.com>
2021-02-17 19:08:22 +0100

    module-info: depend on io.sf.jclf.text module.

--- src/module-info.java
+++ src/module-info.java
@@ -23,7 +23,7 @@ module io.sf.carte.css4j {
 
 	requires transitive java.xml;
 	requires transitive jdk.xml.dom;
-	requires io.sf.jclf;
+	requires io.sf.jclf.text;
 	requires org.apache.commons.codec;
 	requires transitive sac;
 	requires static org.slf4j;
