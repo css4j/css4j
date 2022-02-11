d9ae973fc9c9c130aa6f4e7b684edd35c84ead36
Carlos Amengual <carlosame644+github@gmail.com>
2022-02-11 21:20:18 +0100

    Tests: add a URL parsing test.

--- junit/io/sf/carte/doc/style/css/parser/PropertyParserTest.java
+++ junit/io/sf/carte/doc/style/css/parser/PropertyParserTest.java
@@ -3642,6 +3642,16 @@ public class PropertyParserTest {
 	@Test
 	public void testParsePropertyValueURLSemicolon() throws CSSException, IOException {
 		LexicalUnit lu = parsePropertyValue(
+			"url(data:image/png;base64,MTIzNDU2Nzg5MGFiY2RlZmckJSYvKCk/)");
+		assertEquals(LexicalType.URI, lu.getLexicalUnitType());
+		assertEquals("data:image/png;base64,MTIzNDU2Nzg5MGFiY2RlZmckJSYvKCk/", lu.getStringValue());
+		assertEquals("url('data:image/png;base64,MTIzNDU2Nzg5MGFiY2RlZmckJSYvKCk/')",
+			lu.toString());
+	}
+
+	@Test
+	public void testParsePropertyValueURLSemicolon2() throws CSSException, IOException {
+		LexicalUnit lu = parsePropertyValue(
 			"url(https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,200;0,300;0,400;0,500;0,700;1,400;1,500;1,700&display=swap)");
 		assertEquals(LexicalType.URI, lu.getLexicalUnitType());
 		assertEquals(
