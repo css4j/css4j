87448b7126406731a1e1751560e52e08c928a244
Carlos Amengual <carlosame644+github@gmail.com>
2022-10-16 17:37:38 +0200

    Implement CSS Object Model's `CSS.escape()` method.
    Closes #18

---
+++ src/io/sf/carte/doc/style/css/CSS.java
@@ -0,0 +1,35 @@
+/*
+
+ Copyright (c) 2005-2022, Carlos Amengual.
+
+ SPDX-License-Identifier: BSD-3-Clause
+
+ Licensed under a BSD-style License. You can find the license here:
+ https://css4j.github.io/LICENSE.txt
+
+ */
+
+package io.sf.carte.doc.style.css;
+
+import io.sf.carte.doc.style.css.parser.ParseHelper;
+
+/**
+ * CSS static methods.
+ */
+public class CSS {
+
+	/**
+	 * Escape the given string according to CSS syntax.
+	 * <p>
+	 * For convenience, if the string contains an escape sequence escaping a private
+	 * or unassigned character, the corresponding backslash is not escaped.
+	 * </p>
+	 * 
+	 * @param text the text to escape.
+	 * @return the escaped text.
+	 */
+	public static String escape(String text) {
+		return ParseHelper.safeEscape(text);
+	}
+
+}
