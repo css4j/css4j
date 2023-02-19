46ccb4891217b9a911e52f6a647261c77e9314a4
Carlos Amengual <carlosame644+github@gmail.com>
2023-02-19 16:14:16 +0100

    Migrate to a Maven/Gradle source directory layout
    Closes #21

--- buildSrc/src/main/groovy/CRLFConvertCopy.groovy
+++ buildSrc/src/main/groovy/CRLFConvertCopy.groovy
@@ -15,40 +15,67 @@ import org.gradle.api.tasks.TaskAction
  * </p>
  * <code>
  * tasks.register('lineEndingConvCopy', CRLFConvertCopy) {
- *     from "path/to/file1.txt"
- *     from "path/to/fileN.txt"
+ *   from "path/to/file1.txt"
+ *   from "path/to/fileN.txt"
+ * }
+ * </code>
+ * <p>
+ * You can set a prefix for the files (or a parent directory under
+ * <code>tmp/crlf</code>), if you end the prefix with a slash <code>'/'</code>.
+ * For example:</p>
+ * <code>
+ * tasks.register('prefixedLineEndingConvCopy', CRLFConvertCopy) {
+ *   from "path/to/file1.txt"
+ *   from "path/to/fileN.txt"
+ *   prefix = 'pre/'
  * }
  * </code>
  */
 class CRLFConvertCopy extends DefaultTask {
 
-    private static final String CRLF = "\r\n"
-    private static final String LF = "\n"
+	private static final String CRLF = "\r\n"
+	private static final String LF = "\n"
 
-    private files = []
+	private files = []
+	private String pref = '';
 
-    @TaskAction
-    def action() {
-        def tmpDir = new File(project.buildDir, 'tmp');
-        def destDir = new File(tmpDir, 'crlf');
-        if (!destDir.exists()) {
-            destDir.mkdirs();
-        }
-        files.each { path ->
-            def file = new File(path)
-            if (file.exists()) {
-                String content = file.text
-                String newContent = content.replaceAll(/\r\n/, LF)
-                newContent = newContent.replaceAll(/\n|\r/, CRLF)
-                def dest = new File(destDir, file.name);
-                dest.write(newContent, 'utf-8')
-            } else {
-                logger.warn('File ' + path + ' does not exist.')
-            }
-        }
-    }
+	@TaskAction
+	def action() {
+		def tmpDir = new File(project.buildDir, 'tmp');
+		def destDir = new File(tmpDir, 'crlf');
+		if (!destDir.exists()) {
+			destDir.mkdirs();
+		}
+		def myPref = '';
+		if (pref) {
+			if (pref.endsWith('/')) {
+				destDir = new File(destDir, pref);
+				if (!destDir.exists()) {
+					destDir.mkdirs();
+				}
+			} else {
+				myPref = pref;
+			}
+		}
+		files.each { path ->
+			def file = new File(path)
+			if (file.exists()) {
+				String content = file.text
+				String newContent = content.replaceAll(/\r\n/, LF)
+				newContent = newContent.replaceAll(/\n|\r/, CRLF)
+				def dest = new File(destDir, myPref + file.name);
+				dest.write(newContent, 'utf-8')
+			} else {
+				logger.warn('File ' + path + ' does not exist.')
+			}
+		}
+	}
 
-    def from(String path) {
-        this.files << path
-    }
+	def from(String path) {
+		this.files << path
+	}
+
+	def setPrefix(String prefix) {
+		this.pref = prefix;
+	}
 }
