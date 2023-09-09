c4b507bfa3d44317bb8e1d8ed6bd296c1162372f
Carlos Amengual <carlosame644+github@gmail.com>
2023-09-09 18:31:55 +0200

    CSSOM: improve the mapping from legacy HTML presentational attributes to style

--- src/main/java/io/sf/carte/doc/style/css/property/AttributeToStyle.java
+++ src/main/java/io/sf/carte/doc/style/css/property/AttributeToStyle.java
@@ -14,13 +14,20 @@ package io.sf.carte.doc.style.css.property;
 import io.sf.carte.doc.style.css.CSSStyleDeclaration;
 
 /**
- * Transforms HTML attributes into style declarations.
+ * Transforms legacy HTML attributes into style declarations.
  * 
  * @author Carlos Amengual
  *
  */
 public final class AttributeToStyle {
 
+	/**
+	 * Set a style according to the {@code align} attribute of table elements
+	 * interpreted as a presentational hint.
+	 * 
+	 * @param attribute the attribute value.
+	 * @param style     the style to set.
+	 */
 	public static void align(String attribute, CSSStyleDeclaration style) {
 		if (attribute.equalsIgnoreCase("center") || attribute.equalsIgnoreCase("middle")) {
 			style.setProperty("text-align", "center", null);
@@ -33,6 +40,13 @@ public final class AttributeToStyle {
 		}
 	}
 
+	/**
+	 * Set a style according to the {@code bgcolor} attribute of table elements
+	 * interpreted as a presentational hint.
+	 * 
+	 * @param bgcolorAttr the attribute value.
+	 * @param style       the style to set.
+	 */
 	public static void bgcolor(String bgcolorAttr, CSSStyleDeclaration style) {
 		bgcolorAttr = bgcolorAttr.trim();
 		if (bgcolorAttr.length() != 0) {
@@ -40,28 +54,53 @@ public final class AttributeToStyle {
 		}
 	}
 
+	/**
+	 * Set a style according to the {@code height} attribute of legacy HTML 4.01
+	 * elements interpreted as a presentational hint.
+	 * 
+	 * @param widthAttr the attribute value.
+	 * @param style     the style to set.
+	 */
 	public static void width(String widthAttr, CSSStyleDeclaration style) {
 		int attlen = widthAttr.length();
 		if (attlen != 0) {
 			char c = widthAttr.charAt(attlen - 1);
-			if (Character.isDigit(c)) {
+			if (isDigit(c)) {
 				widthAttr += "px";
 			}
 			style.setProperty("width", widthAttr, null);
 		}
 	}
 
+	private static boolean isDigit(char c) {
+		return c >= 0x30 && c <= 0x39;
+	}
+
+	/**
+	 * Set a style according to the {@code height} attribute of legacy HTML 4.01
+	 * elements interpreted as a presentational hint.
+	 * 
+	 * @param heightAttr the attribute value.
+	 * @param style      the style to set.
+	 */
 	public static void height(String heightAttr, CSSStyleDeclaration style) {
 		int attlen = heightAttr.length();
 		if (attlen != 0) {
 			char c = heightAttr.charAt(attlen - 1);
-			if (Character.isDigit(c)) {
+			if (isDigit(c)) {
 				heightAttr += "px";
 			}
 			style.setProperty("height", heightAttr, null);
 		}
 	}
 
+	/**
+	 * Set a style according to the {@code size} attribute of {@code FONT} elements
+	 * interpreted as a presentational hint.
+	 * 
+	 * @param attribute the attribute value.
+	 * @param style     the style to set.
+	 */
 	public static void face(String attribute, CSSStyleDeclaration style) {
 		attribute = attribute.trim();
 		if (attribute.length() != 0) {
@@ -69,25 +108,73 @@ public final class AttributeToStyle {
 		}
 	}
 
+	/**
+	 * Set a style according to the {@code size} attribute of {@code FONT} elements
+	 * interpreted as a presentational hint.
+	 * 
+	 * @param attribute the attribute value.
+	 * @param style     the style to set.
+	 */
 	public static void size(String attribute, CSSStyleDeclaration style) {
 		attribute = attribute.trim();
 		if (attribute.length() != 0) {
-			style.setProperty("font-size", translateFontSize(attribute), null);
+			String sz = translateFontSize(attribute);
+			if (!sz.isEmpty()) {
+				style.setProperty("font-size", sz, null);
+			}
 		}
 	}
 
 	private static String translateFontSize(String size) {
 		// It is assumed was previously checked that size.length() 
 		// is greater than zero.
-		if(size.charAt(0) == '+') {
+		if (size.charAt(0) == '+') {
 			return "larger";
-		} else if(size.charAt(0) == '-') {
+		} else if (size.charAt(0) == '-') {
 			return "smaller";
 		} else {
+			int sz;
+			try {
+				sz = Integer.parseInt(size);
+			} catch (NumberFormatException e) {
+				return "";
+			}
+			switch (sz) {
+			case 1:
+				size = "8px";
+				break;
+			case 2:
+				size = "10px";
+				break;
+			case 3:
+				size = "12px";
+				break;
+			case 4:
+				size = "14px";
+				break;
+			case 5:
+				size = "18px";
+				break;
+			case 6:
+				size = "24px";
+				break;
+			case 7:
+				size = "28px";
+				break;
+			default:
+				size = "";
+			}
 			return size;
 		}
 	}
 
+	/**
+	 * Set a style according to the {@code color} attribute of {@code FONT} elements
+	 * interpreted as a presentational hint.
+	 * 
+	 * @param attribute the attribute value.
+	 * @param style     the style to set.
+	 */
 	public static void color(String attribute, CSSStyleDeclaration style) {
 		attribute = attribute.trim();
 		if (attribute.length() != 0) {
@@ -95,12 +182,19 @@ public final class AttributeToStyle {
 		}
 	}
 
+	/**
+	 * Set a style according to the {@code border} attribute of table elements
+	 * interpreted as a presentational hint.
+	 * 
+	 * @param attribute the attribute value.
+	 * @param style     the style to set.
+	 */
 	public static boolean border(String attribute, CSSStyleDeclaration style) {
 		attribute = attribute.trim();
 		int attlen = attribute.length();
 		if (attlen != 0) {
 			char c = attribute.charAt(attlen - 1);
-			if (Character.isDigit(c)) {
+			if (isDigit(c)) {
 				attribute += "px";
 			}
 			style.setProperty("border-top-width", attribute, null);
@@ -112,6 +206,13 @@ public final class AttributeToStyle {
 		return false;
 	}
 
+	/**
+	 * Set a style according to the {@code bordercolor} attribute of table elements
+	 * interpreted as a presentational hint.
+	 * 
+	 * @param attribute the attribute value.
+	 * @param style     the style to set.
+	 */
 	public static void borderColor(String attribute, CSSStyleDeclaration style) {
 		attribute = attribute.trim();
 		int attlen = attribute.length();
@@ -123,18 +224,32 @@ public final class AttributeToStyle {
 		}
 	}
 
+	/**
+	 * Set a style according to the {@code cell-spacing} attribute of table elements
+	 * interpreted as a presentational hint.
+	 * 
+	 * @param attribute the attribute value.
+	 * @param style     the style to set.
+	 */
 	public static void cellSpacing(String attribute, CSSStyleDeclaration style) {
 		attribute = attribute.trim();
 		int attlen = attribute.length();
 		if (attlen != 0) {
 			char c = attribute.charAt(attlen - 1);
-			if (Character.isDigit(c)) {
+			if (isDigit(c)) {
 				attribute += "px";
 			}
 			style.setProperty("border-spacing", attribute, null);
 		}
 	}
 
+	/**
+	 * Set a style according to the {@code background} attribute of table elements
+	 * interpreted as a presentational hint.
+	 * 
+	 * @param attribute the attribute value.
+	 * @param style     the style to set.
+	 */
 	public static void background(String attribute, CSSStyleDeclaration style) {
 		attribute = attribute.trim();
 		if (attribute.length() != 0) {
@@ -142,19 +257,41 @@ public final class AttributeToStyle {
 		}
 	}
 
+	/**
+	 * Set a style according to the {@code hspace} attribute of the {@code IMG}
+	 * element interpreted as a presentational hint.
+	 * 
+	 * @param attribute the attribute value.
+	 * @param style     the style to set.
+	 */
 	public static void hspace(String attribute, CSSStyleDeclaration style) {
 		attribute = attribute.trim();
 		int attlen = attribute.length();
 		if (attlen != 0) {
+			char c = attribute.charAt(attlen - 1);
+			if (isDigit(c)) {
+				attribute += "px";
+			}
 			style.setProperty("margin-right", attribute, null);
 			style.setProperty("margin-left", attribute, null);
 		}
 	}
 
+	/**
+	 * Set a style according to the {@code vspace} attribute of the {@code IMG}
+	 * element interpreted as a presentational hint.
+	 * 
+	 * @param attribute the attribute value.
+	 * @param style     the style to set.
+	 */
 	public static void vspace(String attribute, CSSStyleDeclaration style) {
 		attribute = attribute.trim();
 		int attlen = attribute.length();
 		if (attlen != 0) {
+			char c = attribute.charAt(attlen - 1);
+			if (isDigit(c)) {
+				attribute += "px";
+			}
 			style.setProperty("margin-top", attribute, null);
 			style.setProperty("margin-bottom", attribute, null);
 		}
---
+++ src/test/resources/io/sf/carte/doc/style/css/property/AttributeToStyleTest.java
@@ -0,0 +1,187 @@
+/*
+
+ Copyright (c) 2005-2023, Carlos Amengual.
+
+ SPDX-License-Identifier: BSD-3-Clause
+
+ Licensed under a BSD-style License. You can find the license here:
+ https://css4j.github.io/LICENSE.txt
+
+ */
+
+package io.sf.carte.doc.style.css.property;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import io.sf.carte.doc.style.css.CSSStyleDeclaration;
+import io.sf.carte.doc.style.css.om.AbstractCSSStyleSheet;
+import io.sf.carte.doc.style.css.om.TestCSSStyleSheetFactory;
+
+public class AttributeToStyleTest {
+
+	static AbstractCSSStyleSheet sheet;
+
+	CSSStyleDeclaration style;
+
+	@BeforeAll
+	public static void setUpBeforeAll() {
+		TestCSSStyleSheetFactory factory = new TestCSSStyleSheetFactory();
+		sheet = factory.createStyleSheet(null, null);
+	}
+
+	@BeforeEach
+	public void setUp() {
+		style = sheet.createStyleDeclaration();
+	}
+
+	@Test
+	void testAlign() {
+		AttributeToStyle.align("middle", style);
+		assertEquals("center", style.getPropertyValue("text-align"));
+
+		AttributeToStyle.align("justify", style);
+		assertEquals("justify", style.getPropertyValue("text-align"));
+
+		AttributeToStyle.align("left", style);
+		assertEquals("left", style.getPropertyValue("text-align"));
+
+		AttributeToStyle.align("right", style);
+		assertEquals("right", style.getPropertyValue("text-align"));
+	}
+
+	@Test
+	void testBgcolor() {
+		AttributeToStyle.bgcolor("orange", style);
+		assertEquals("orange", style.getPropertyValue("background-color"));
+	}
+
+	@Test
+	void testWidth() {
+		AttributeToStyle.width("30%", style);
+		assertEquals("30%", style.getPropertyValue("width"));
+
+		AttributeToStyle.width("10", style);
+		assertEquals("10px", style.getPropertyValue("width"));
+
+		AttributeToStyle.width("19", style);
+		assertEquals("19px", style.getPropertyValue("width"));
+	}
+
+	@Test
+	void testHeight() {
+		AttributeToStyle.height("30%", style);
+		assertEquals("30%", style.getPropertyValue("height"));
+
+		AttributeToStyle.height("10", style);
+		assertEquals("10px", style.getPropertyValue("height"));
+
+		AttributeToStyle.height("19", style);
+		assertEquals("19px", style.getPropertyValue("height"));
+	}
+
+	@Test
+	void testFace() {
+		AttributeToStyle.face("Arial, Helvetica", style);
+		assertEquals("Arial, Helvetica", style.getPropertyValue("font-family"));
+	}
+
+	@Test
+	void testSize() {
+		AttributeToStyle.size("1", style);
+		assertEquals("8px", style.getPropertyValue("font-size"));
+
+		AttributeToStyle.size("2", style);
+		assertEquals("10px", style.getPropertyValue("font-size"));
+
+		AttributeToStyle.size("3", style);
+		assertEquals("12px", style.getPropertyValue("font-size"));
+
+		AttributeToStyle.size("4", style);
+		assertEquals("14px", style.getPropertyValue("font-size"));
+
+		AttributeToStyle.size("5", style);
+		assertEquals("18px", style.getPropertyValue("font-size"));
+
+		AttributeToStyle.size("6", style);
+		assertEquals("24px", style.getPropertyValue("font-size"));
+
+		AttributeToStyle.size("7", style);
+		assertEquals("28px", style.getPropertyValue("font-size"));
+
+		AttributeToStyle.size("+1", style);
+		assertEquals("larger", style.getPropertyValue("font-size"));
+
+		AttributeToStyle.size("-1", style);
+		assertEquals("smaller", style.getPropertyValue("font-size"));
+	}
+
+	@Test
+	void testSizeInvalid() {
+		AttributeToStyle.size("100", style);
+		assertEquals(0, style.getPropertyValue("font-size").length());
+
+		AttributeToStyle.size("invalid", style);
+		assertEquals(0, style.getPropertyValue("font-size").length());
+	}
+
+	@Test
+	void testColor() {
+		AttributeToStyle.color("blue", style);
+		assertEquals("blue", style.getPropertyValue("color"));
+	}
+
+	@Test
+	void testBorder() {
+		AttributeToStyle.border("6", style);
+		assertEquals("6px", style.getPropertyValue("border-top-width"));
+		assertEquals("6px", style.getPropertyValue("border-right-width"));
+		assertEquals("6px", style.getPropertyValue("border-bottom-width"));
+		assertEquals("6px", style.getPropertyValue("border-left-width"));
+
+		AttributeToStyle.border("thick", style);
+		assertEquals("thick", style.getPropertyValue("border-top-width"));
+		assertEquals("thick", style.getPropertyValue("border-right-width"));
+		assertEquals("thick", style.getPropertyValue("border-bottom-width"));
+		assertEquals("thick", style.getPropertyValue("border-left-width"));
+	}
+
+	@Test
+	void testBorderColor() {
+		AttributeToStyle.borderColor("blue", style);
+		assertEquals("blue", style.getPropertyValue("border-top-color"));
+		assertEquals("blue", style.getPropertyValue("border-right-color"));
+		assertEquals("blue", style.getPropertyValue("border-bottom-color"));
+		assertEquals("blue", style.getPropertyValue("border-left-color"));
+	}
+
+	@Test
+	void testCellSpacing() {
+		AttributeToStyle.cellSpacing("2", style);
+		assertEquals("2px", style.getPropertyValue("border-spacing"));
+	}
+
+	@Test
+	void testBackground() {
+		AttributeToStyle.background("url(image.jpg)", style);
+		assertEquals("url('image.jpg')", style.getPropertyValue("background-image"));
+	}
+
+	@Test
+	void testHspace() {
+		AttributeToStyle.hspace("16", style);
+		assertEquals("16px", style.getPropertyValue("margin-right"));
+		assertEquals("16px", style.getPropertyValue("margin-left"));
+	}
+
+	@Test
+	void testVspace() {
+		AttributeToStyle.vspace("16", style);
+		assertEquals("16px", style.getPropertyValue("margin-top"));
+		assertEquals("16px", style.getPropertyValue("margin-bottom"));
+	}
+
+}
