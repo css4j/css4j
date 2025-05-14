ec545908a5fb098adedd037e911ea33c8f9f3a71
Carlos Amengual <carlosame644+github@gmail.com>
2025-05-14 22:43:22 +0200

    Put the SPDX header in a separate comment

--- src/test/java/io/sf/carte/doc/style/css/om/BorderRadiusBuilderTest.java
+++ src/test/java/io/sf/carte/doc/style/css/om/BorderRadiusBuilderTest.java
@@ -2,12 +2,12 @@
 
  Copyright (c) 2005-2025, Carlos Amengual.
 
- SPDX-License-Identifier: BSD-3-Clause
-
  Licensed under a BSD-style License. You can find the license here:
  https://css4j.github.io/LICENSE.txt
 
  */
+
+// SPDX-License-Identifier: BSD-3-Clause
 
 package io.sf.carte.doc.style.css.om;
 
