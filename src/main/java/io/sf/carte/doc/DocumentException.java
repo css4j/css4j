e9680233fc4d1edec6277ade27f42afce127c55d
Carlos Amengual <carlosame644+github@gmail.com>
2025-05-26 15:38:40 +0200

    Follow-up to ec545908a (some SPDX headers still weren't in their own comment)

--- src/main/java/io/sf/carte/doc/DocumentException.java
+++ src/main/java/io/sf/carte/doc/DocumentException.java
@@ -2,12 +2,12 @@
 
  Copyright (c) 1998-2025, Carlos Amengual.
 
- SPDX-License-Identifier: BSD-3-Clause
-
  Licensed under a BSD-style License. You can find the license here:
  https://css4j.github.io/LICENSE.txt
 
  */
+
+// SPDX-License-Identifier: BSD-3-Clause
 
 package io.sf.carte.doc;
 
@@ -16,9 +16,6 @@ package io.sf.carte.doc;
  * <p>
  * Generally contains domain-specific exceptions that give more information,
  * like <code>SAXException</code>s or implementation-specific exceptions.
- * 
- * @author Carlos Amengual
- * @version 1.01
  */
 public class DocumentException extends Exception {
 
